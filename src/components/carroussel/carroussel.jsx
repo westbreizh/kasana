fe126adf33ff834b05ceee0665efe8cd1de43e8a
aurélien <herbreteauaurelien@hotmail.fr>
2022-10-06 12:15:30 +0200

    carroussel en construction on va manger

---
+++ src/components/carroussel/carroussel.jsx
@@ -0,0 +1,19 @@
+import  { useState } from "react"
+import leftArrow from "../../assets/vector-left.png"
+import rightArrow from "../../assets/vector-left.png"
+
+
+
+export default function Carroussel(){
+
+
+  
+
+    return (
+
+    <h1>hello</h1>
+
+    
+    )
+    
+}
--- src/components/pages/housing/Housing.jsx
+++ src/components/pages/housing/Housing.jsx
@@ -1,13 +1,37 @@
-import Header from "../../header/Header";
+import { useEffect, useState } from 'react';
+import Header from "../../header/Header"
 import Footer from "../../footer/Footer"
+import Carroussel from "../../carroussel/carroussel"
 
 export default function Housing() {
 
+  const [housingData, setHousingData] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json', {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+              }
+      
+          }).then(res => res.json())
+            .then(result => setHousingData(result))
+            .catch((error) => console.log(error));
+    }, [])
   return ( 
+
     <div>
+
       <Header />
-      <h1> ma page logement en construction </h1>
+
+      <body className="body">
+
+        <Carroussel />
+
+      </body>
+
       <Footer />
+      
     </div>
   )
 }
