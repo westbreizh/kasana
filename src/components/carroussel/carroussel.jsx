65e55566f70f3bfebce266de11deff5f7a5dcca3
aurélien <herbreteauaurelien@hotmail.fr>
2022-10-07 07:25:03 +0200

    logique carroussel ok

--- src/components/carroussel/carroussel.jsx
+++ src/components/carroussel/carroussel.jsx
@@ -1,26 +1,40 @@
 import  { useState } from "react"
-import leftArrow from "../../assets/vector-left.png"
-import rightArrow from "../../assets/vector-left.png"
-
-
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 
 export default function Carroussel(props){
 
-const [index, setIndex] = useState(0)
+let [index, setIndex] = useState(0)
 const arrayPictures= props.pictures
 const picture = arrayPictures[index]
+const lenghtArray = arrayPictures.length
 
+function indexLessOne() {
+  index===0? index=lenghtArray-1 : index-=1;
+  setIndex(index);
+}
 
-
+function indexAndOne() {
+    index===lenghtArray-1? index=0 : index+=1;
+    setIndex(index);
+  }
 
 
     return (
 
-    <div>
-    
-    <img src={picture} alt="différentes vues du logement" className='carroussel_picture'/>
+        <div className="carroussel_container">
 
-    </div>
+                <img src={picture} alt="différentes vues du logement" className='carroussel_picture'/>
+        
+                { lenghtArray>1 &&
+
+                    <div className="icon_container">
+                        <NavigateBeforeIcon onClick= {() => indexLessOne() } className="button_container" />
+                        <NavigateNextIcon onClick= {() => indexAndOne() } className="button_container" />
+                        <p className='caroussel_count'>{(index+1) + '/' + lenghtArray}</p>
+                    </div>
+                } 
+        </div>
     )
     
 }
